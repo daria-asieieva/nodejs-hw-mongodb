e02af623c0ebbd21cd0c30ebd75aa292cdd20e78
Daria <dariaasieieva@gmail.com>
2025-03-09 18:10:12 +0100

    finish

---
+++ src/controllers/contacts.js
@@ -0,0 +1,71 @@
+// src/controllers/contacts.js
+import createError from 'http-errors';
+import { 
+  getAllContacts as getAllContactsService, 
+  getContactById as getContactByIdService,
+  createContact as createContactService,
+  updateContact as updateContactService,
+  deleteContact as deleteContactService 
+} from '../services/contacts.js';
+
+export const getAllContacts = async (req, res) => {
+  const contacts = await getAllContactsService();
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts',
+    data: contacts,
+  });
+};
+
+export const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  console.log('Received contactId:', contactId);
+
+  const contact = await getContactByIdService(contactId);
+
+  if (!contact) {
+    throw createError(404, "Contact not found");
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}`,
+    data: contact,
+  });
+};
+
+export const createContact = async (req, res) => {
+  const newContact = await createContactService(req.body);
+  
+  res.status(201).json({
+    status: 201,
+    message: "Successfully created a contact!",
+    data: newContact
+  });
+};
+
+export const updateContact = async (req, res) => {
+  const { contactId } = req.params;
+  const updatedContact = await updateContactService(contactId, req.body);
+  
+  if (!updatedContact) {
+    throw createError(404, "Contact not found");
+  }
+  
+  res.status(200).json({
+    status: 200,
+    message: "Successfully patched a contact!",
+    data: updatedContact
+  });
+};
+
+export const deleteContact = async (req, res) => {
+  const { contactId } = req.params;
+  const deletedContact = await deleteContactService(contactId);
+  
+  if (!deletedContact) {
+    throw createError(404, "Contact not found");
+  }
+  
+  res.status(204).send();
+};
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -7,3 +7,19 @@ export const getAllContacts = async () => {
 export const getContactById = async (contactId) => {
   return await ContactsCollections.findById(contactId);
 };
+
+export const createContact = async (contactData) => {
+  return await ContactsCollections.create(contactData);
+};
+
+export const updateContact = async (contactId, contactData) => {
+  return await ContactsCollections.findByIdAndUpdate(
+    contactId,
+    contactData,
+    { new: true }
+  );
+};
+
+export const deleteContact = async (contactId) => {
+  return await ContactsCollections.findByIdAndDelete(contactId);
+};
