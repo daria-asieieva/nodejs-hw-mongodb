bcf0347e53af11a252f925561271ac5df4ecd5ce
Daria <dariaasieieva@gmail.com>
2025-03-19 16:34:42 +0100

    finish

--- src/schemas/contacts.js
+++ src/schemas/contacts.js
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-export const createContactsSchema = Joi.object({
+export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().min(3).max(20),
@@ -13,7 +13,7 @@ export const createContactsSchema = Joi.object({
     .default('personal'),
 });
 
-export const updateContactsSchema = Joi.object({
+export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().min(3).max(20).email(),
