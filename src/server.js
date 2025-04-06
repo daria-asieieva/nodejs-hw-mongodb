b1e2a2223e0b5c32253b8a6594b23dedca71bcc1
Daria <dariaasieieva@gmail.com>
2025-04-06 16:54:06 +0200

    finish

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { pinoHttp } from 'pino-http';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
 import { getEnvVar } from './utils/getEnvVar.js';
 import contactsRouter from './routers/contacts.js';
 import authRouter from './routers/auth.js';
@@ -12,6 +14,18 @@ const PORT = Number(getEnvVar('PORT', '3000'));
 
 const setUpServer = () => {
   const app = express();
+
+  
+  let swaggerDocument;
+  try {
+    
+    const swaggerPath = new URL('../docs/swagger.json', import.meta.url);
+    swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, 'utf8'));
+  } catch (error) {
+    console.log(error);
+    console.log('Swagger JSON not found. API docs may not be available.');
+    swaggerDocument = { info: { title: 'API Documentation', version: '1.0.0' } };
+  }
 
   app.use(express.json());
   app.use(cookieParser());
@@ -32,6 +46,9 @@ const setUpServer = () => {
       message: 'Hello Mentor',
     });
   });
+
+  
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
